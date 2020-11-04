37606507c86f432367cdcd3c1fa699a5b8144376
Chris Kabana <chris.kabana@gmail.com>
2020-11-04 06:25:22 -0600

    setting up service-worker

---
+++ public/db.js
@@ -0,0 +1,10 @@
+// placeholder
+
+const indexedDB =
+    window.indexedDB ||
+    window.mozIndexedDB ||
+    window.webkitIndexedDB ||
+    window.msIndexedDB ||
+    window.shimIndexedDB;
+
+    let db;
