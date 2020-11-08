7691146da1f37250fbce5993499cbaf1aabe5db8
Chris Kabana <chris.kabana@gmail.com>
2020-11-07 19:49:08 -0600

    seems to be working

--- public/index.js
+++ public/index.js
@@ -1,11 +1,11 @@
-// if ('serviceWorker' in navigator) {
-//   window.addEventListener('load', () => {
-//     navigator.serviceWorker.register('/service-worker.js')
-//     .then((reg) => {
-//       console.log('Service worker registered.', reg);
-//     });
-//   });
-// }
+if ('serviceWorker' in navigator) {
+  window.addEventListener('load', () => {
+    navigator.serviceWorker.register('/service-worker.js')
+    .then((reg) => {
+      console.log('Service worker registered.', reg);
+    });
+  });
+}
 
 let transactions = [];
 let myChart;
@@ -17,7 +17,7 @@ fetch("/api/transaction")
   .then(data => {
     // save db data on global variable
     transactions = data;
-
+    console.log(data);
     populateTotal();
     populateTable();
     populateChart();
